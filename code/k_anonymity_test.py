44c3faea03dc0c700e3276bca043d2257af69915
Muhammad Haris Iqbal <123210708+harisiqbal10@users.noreply.github.com>
2023-09-27 07:20:42 +0500

    Update k_anonymity_test.py

--- code/k_anonymity_test.py
+++ code/k_anonymity_test.py
@@ -4,12 +4,16 @@ def main():
     data_path = "../data/Flight_DataSet.csv"
     anon = Anonymization(data_path)
 
+    sensitive_attribute = "Flight Status"
+
     k_values = [7, 20, 50]
     l_values = [3, 6, 15]
     for k, l in zip(k_values, l_values):
         anon.anonymize(k, l)
         anon.save_to_csv(f"../data/anonymized_data_k_{k}.csv")
-        utility = anon.measure_utility()
+         # Calculate KL-divergence-based utility for Flight Status
+        utility = anon.measure_utility(sensitive_attribute)
+      
         print(f"Utility for k={k}, and l={l}: {utility}")
 
 if __name__ == "__main__":
